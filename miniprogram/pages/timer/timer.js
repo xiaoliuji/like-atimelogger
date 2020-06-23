ef3238fb1660e70c2d838a297d66472706d8271d
xiaoliuji <31358885+xiaoliuji@users.noreply.github.com>
2020-06-23 22:07:58 +0800

    搭建完第一个页面

---
+++ miniprogram/pages/timer/timer.js
@@ -0,0 +1,112 @@
+// pages/timer/timer.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    timerItems: [
+      {
+        id: "0",
+        timerImg: "/assets/images/icon-repository/booking.png",
+        timerText: "预定"
+      },
+      {
+        id: "1",
+        timerImg: "/assets/images/icon-repository/home.png",
+        timerText: "在家"
+      },
+      {
+        id: "2",
+        timerImg: "/assets/images/icon-repository/label.png",
+        timerText: "标签"
+      },
+      {
+        id: "3",
+        timerImg: "/assets/images/icon-repository/money.png",
+        timerText: "理财"
+      },
+      {
+        id: "4",
+        timerImg: "/assets/images/icon-repository/research.png",
+        timerText: "研究"
+      },
+      {
+        id: "5",
+        timerImg: "/assets/images/icon-repository/star.png",
+        timerText: "星标"
+      },
+      {
+        id: "6",
+        timerImg: "/assets/images/icon-repository/study.png",
+        timerText: "学习"
+      },
+      {
+        id: "7",
+        timerImg: "/assets/images/icon-repository/flow.png",
+        timerText: "心流"
+      },
+      {
+        id: "8",
+        timerImg: "/assets/images/icon-repository/work.png",
+        timerText: "工作"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
